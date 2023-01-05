7b3bc7feef92d3dd88429e84b102857575f6dccb
Isaac <fimbresisaac23@gmail.com>
2023-01-05 00:17:52 -0800

    minor change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import { customAlphabet } from 'nanoid';
 import { useRouter } from "next/router";
 import { Modal } from "../components/Modal";
 import { useState } from "react";
-import Image from "next/image";
 
 const nanoId = customAlphabet('abcdefghijklmnopqrstuvwxyz1234567890', 10);
 
@@ -47,7 +46,7 @@ const Home: NextPage = () => {
             </div>
             <div className="px-4">
               <div className="text-white text-center mb-8 text-xl font-semibold">Join Private Chat Room</div>
-              <input autoFocus={true} className="w-full py-2 px-2 rounded-full mb-3 bg-neutral-600 focus:ring-white text-white" value={roomId} onChange={(e) => setRoomId(e.currentTarget.value)} />
+              <input placeholder="Enter the chat room id..." autoFocus={true} className="w-full py-2 px-5 rounded-full mb-3 bg-neutral-600 focus:ring-white text-white" value={roomId} onChange={(e) => setRoomId(e.currentTarget.value)} />
               <button className="w-full py-3 rounded-full bg-gradient-to-r from-sky-600 to-indigo-700 text-white font-semibold transition-opacity duration-300 hover:opacity-30" type="submit">Go To The Chat Room</button>
             </div>
           </form>

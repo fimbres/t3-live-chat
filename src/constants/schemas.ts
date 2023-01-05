e728dbf4cf26404dfb00fcf543c48f7796af16f7
Isaac <fimbresisaac23@gmail.com>
2023-01-05 00:17:23 -0800

    Making roomId optional

--- src/constants/schemas.ts
+++ src/constants/schemas.ts
@@ -1,14 +1,14 @@
 import z from "zod";
 
 export const sendMessageSchema = z.object({
-    roomId: z.string(),
+    roomId: z.string().optional(),
     message: z.string(),
 });
 
 const messageSchema = z.object({
     id: z.string(),
     message: z.string(),
-    roomId: z.string(),
+    roomId: z.string().optional(),
     sentAt: z.date(),
     sender: z.object({
         name: z.string()
@@ -17,5 +17,5 @@ const messageSchema = z.object({
 
 export type Message = z.TypeOf<typeof messageSchema>;
 export const messageSubSchema = z.object({
-    roomId: z.string()
+    roomId: z.string().optional()
 });

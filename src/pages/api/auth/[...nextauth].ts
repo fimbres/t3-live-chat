d753f6a1cf0b957f415007195d6a40d8880dff82
Isaac <fimbresisaac23@gmail.com>
2023-01-04 22:43:15 -0800

    fixing authentication

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,55 +1,47 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-// Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { customAlphabet } from "nanoid";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
 
+const nanoId = customAlphabet('abcdefghijklmnopqrstuvwxyz1234567890', 10);
+
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
   callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user = user;
-      }
+    async session({ session, user }) {
       return session;
     },
   },
-  // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
         name: {
-          label: "Name",
+          label: "Username",
           type: "text",
-          placeholder: "Enter your name"
+          placeholder: "Enter your username"
         },
         password: {
           label: "Password",
-          type: "password"
+          type: "password",
+          placeholder: "Enter your password"
         }
       },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: credentials?.name, password: credentials?.password }
+      async authorize(credentials, _) {
+        const user = { id: nanoId(), name: credentials?.name, password: credentials?.password }
   
         if (user) {
-          // Any object returned will be saved in `user` property of the JWT
           return user
         } else {
-          // If you return null then an error will be displayed advising the user to check their details.
           return null
-  
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
       }
     })
-    // ...add more providers here
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt"
   }
